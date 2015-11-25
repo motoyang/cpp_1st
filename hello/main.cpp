014d8cf16e7ed907ec20265d1d9ca8be3d9b2487
xt <xt@xtdeMacBook-Pro.local>
2015-11-25 21:09:35 +0800

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by xt on 15/11/25.
+//  Copyright (c) 2015年 xt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
